ae7c1cfe38a4d2727719769878c241a926a84f44
TsHuang <tshuang1983@gmail.com>
2017-11-18 11:04:03 +0800

    update run_all.sh to build folders to save checkpoints and loss

--- myCodes/run_all.sh
+++ myCodes/run_all.sh
@@ -1,2 +1,7 @@
+mkdir log_topdown
 python train.py --id td --caption_model topdown  --checkpoint_path log_topdown --max_epochs 10
+mv loss.txt log_topdown/loss.txt
+
+#mkdir log_sat
 #python train.py --id sat --caption_model show_attend_tell  --checkpoint_path log_sat --max_epochs 10
+#mv loss.txt log_sat/loss.txt
